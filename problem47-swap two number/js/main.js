47d9d9208c023b7944dd01adce4dbc51dce6693a
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:40:30 +0530

    adding files.

---
+++ problem47-swap two number/js/main.js
@@ -0,0 +1,10 @@
+var a=prompt("enter the value of a");
+a=parseInt(a);
+var b=prompt("enter the value of b");
+b=parseInt(b);
+var temp;
+var temp=a;
+var a=b;
+var b=temp;
+alert("before swap"+"\n"+"value of a is"+a+"\n"+"value of b is"+b);
+alert("after swap"+"\n"+"value of b is"+b+"\n"+"value of a is"+a);
